fa535aa983c8b19690d26e47d0ad114bf8511587
dhoncrisley <dhoncrisley.lins@alive.app.br>
2023-11-08 18:38:50 -0300

    fix(orientation): save requested orientation on ad

--- ad_library/src/main/kotlin/com/adgrowth/internal/integrations/adserver/services/GetAppMetaService.kt
+++ ad_library/src/main/kotlin/com/adgrowth/internal/integrations/adserver/services/GetAppMetaService.kt
@@ -6,19 +6,14 @@ import com.adgrowth.internal.integrations.adserver.entities.AppMetaData
 import com.adgrowth.internal.integrations.adserver.exceptions.APIIOException
 import com.adgrowth.internal.integrations.adserver.helpers.IOErrorHandler
 
-class GetAppMetaService(private val clientKey: String) {
+class GetAppMetaService {
 
     private val mHttpClient: HttpClient = HttpClient()
 
     @Throws(AdRequestException::class)
     fun run(): AppMetaData {
-
-        val params = HashMap<String, Any>()
-
-        params["client_key"] = clientKey
-
         return try {
-            val response = mHttpClient["/adserver/api/app_meta", params]
+            val response = mHttpClient["/adserver/api/app_meta", null]
             AppMetaData(response)
         } catch (e: APIIOException) {
             throw IOErrorHandler.handle(e)
