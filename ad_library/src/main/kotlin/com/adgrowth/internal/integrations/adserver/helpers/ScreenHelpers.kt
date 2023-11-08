5f838a943cf407d48b757be371ffbe7c4a823b8b
dhoncrisley <dhoncrisley.lins@alive.app.br>
2023-11-08 18:29:39 -0300

    fix(orientation): save requested orientation on ad

--- ad_library/src/main/kotlin/com/adgrowth/internal/integrations/adserver/helpers/ScreenHelpers.kt
+++ ad_library/src/main/kotlin/com/adgrowth/internal/integrations/adserver/helpers/ScreenHelpers.kt
@@ -1,12 +1,26 @@
 package com.adgrowth.internal.integrations.adserver.helpers
 
+
+import android.app.Activity
 import android.content.Context
+import android.content.pm.ActivityInfo
 import android.content.res.Configuration
+import com.adgrowth.adserver.enums.AdOrientation
+
 
 object ScreenHelpers {
     @JvmStatic
-    fun getOrientation(context: Context): String {
+    fun getOrientation(context: Context): AdOrientation {
         val orientation = context.resources.configuration.orientation
-        return if (orientation == Configuration.ORIENTATION_LANDSCAPE) "LANDSCAPE" else "PORTRAIT"
+        return if (orientation == Configuration.ORIENTATION_LANDSCAPE) AdOrientation.LANDSCAPE else AdOrientation.PORTRAIT
+    }
+
+    fun setOrientation(context: Activity, orientation: AdOrientation) {
+        when (orientation) {
+            AdOrientation.PORTRAIT -> context.requestedOrientation =
+                ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            AdOrientation.LANDSCAPE -> context.requestedOrientation =
+                ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
     }
 }
