060ee756d4d932980394e3f9a1cad2ecd5067e1f
Dhoncrisley Lins <dhoncrisley.lins@alive.app.br>
2024-03-18 13:24:27 -0300

    fix: add delay of 2 seconds before call api events for better ui performance

--- ad_library/src/main/kotlin/com/adgrowth/internal/integrations/adserver/services/SendAdEventService.kt
+++ ad_library/src/main/kotlin/com/adgrowth/internal/integrations/adserver/services/SendAdEventService.kt
@@ -1,5 +1,6 @@
 package com.adgrowth.internal.integrations.adserver.services
 
+import android.util.Log
 import com.adgrowth.adserver.BuildConfig
 import com.adgrowth.internal.http.HttpClient
 import com.adgrowth.internal.integrations.adserver.entities.Ad
@@ -10,20 +11,35 @@ import com.adgrowth.internal.integrations.adserver.helpers.AdUriHelpers.getUniqu
 import com.adgrowth.internal.integrations.adserver.helpers.AdUriHelpers.openUrl
 import com.adgrowth.internal.integrations.adserver.helpers.AdUriHelpers.replaceURISnippets
 import com.adgrowth.internal.interfaces.managers.AdManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import com.adgrowth.internal.integrations.adserver.services.interfaces.SendAdEventService as ISendAdEventService
 import java.util.*
 
 class SendAdEventService(override val manager: AdManager<*, *>) : ISendAdEventService(manager) {
 
     private val mHttpClient: HttpClient = HttpClient()
+    private val scope = CoroutineScope(Dispatchers.IO)
 
     override fun run(type: AdEventType, ad: Ad) {
         val context = manager.context
         val uniqueId = getUniqueId()
-        
-        Thread {
-            when (type) {
-                AdEventType.VIEW -> {
+
+        if (type == AdEventType.CLICK) {
+            try {
+                openUrl(context, ad.actionUrl, manager.ipAddress, uniqueId)
+            } catch (e: Exception) {
+                // third-party url errors doesn't matter
+            }
+        }
+
+        val timer = Timer()
+
+        val delayedThreadStartTask = object : TimerTask() {
+            override fun run() {
+                val impressionTask = scope.async {
                     try {
                         val mThirdPartyHttpClient = HttpClient(
                             replaceURISnippets(ad.impressionUrl, ad.ipAddress, uniqueId)
@@ -32,39 +48,35 @@ class SendAdEventService(override val manager: AdManager<*, *>) : ISendAdEventSe
                     } catch (e: APIIOException) {
                         // third-party url errors doesn't matter
                     }
-
                 }
 
-                AdEventType.CLICK -> {
+                val eventTask = scope.async {
                     try {
-                        openUrl(context, ad.actionUrl, manager.ipAddress, uniqueId)
-                    } catch (e: Exception) {
-                        // third-party url errors doesn't matter
+                        val params = HashMap<String, Any>()
+
+                        params["unit_id"] = manager.unitId
+                        params["type"] = type.toString()
+                        params["ad_id"] = manager.adId
+                        params["ip"] = manager.ipAddress
+                        params["click_id"] = uniqueId
+                        params["site_id"] = InitializationManager.APP_META_DATA.appId
+                        params["advertising_id"] = InitializationManager.ADVERTISING_ID
+
+                        mHttpClient["/ads/adverts/events", params]
+                    } catch (e: APIIOException) {
+                        if (BuildConfig.DEBUG) {
+                            e.printStackTrace()
+                        }
                     }
+                    return@async
+                }
+
+                scope.launch {
+                    impressionTask.start()
+                    eventTask.start()
                 }
             }
-        }.start()
-
-        Thread {
-            try {
-                val params = HashMap<String, Any>()
-
-                params["unit_id"] = manager.unitId
-                params["type"] = type.toString()
-                params["ad_id"] = manager.adId
-                params["ip"] = manager.ipAddress
-                params["click_id"] = uniqueId
-                params["site_id"] = InitializationManager.APP_META_DATA.appId
-                params["advertising_id"] = InitializationManager.ADVERTISING_ID
-
-                mHttpClient["/ads/adverts/events", params]
-            } catch (e: APIIOException) {
-                // TODO: ignore?
-                if (BuildConfig.DEBUG) {
-                    e.printStackTrace()
-                }
-            }
-        }.start()
-
+        }
+        timer.schedule(delayedThreadStartTask, 2000)
     }
 }
