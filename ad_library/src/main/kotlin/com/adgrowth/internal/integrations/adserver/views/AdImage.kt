5023aefbb7dcdd09b998e95af0f079772ff022f9
Dhoncrisley Lins <dhoncrisley.lins@alive.app.br>
2024-04-04 13:04:51 -0300

    fix: rollback with webview for rendering medias

--- ad_library/src/main/kotlin/com/adgrowth/internal/integrations/adserver/views/AdImage.kt
+++ ad_library/src/main/kotlin/com/adgrowth/internal/integrations/adserver/views/AdImage.kt
@@ -1,42 +1,97 @@
 package com.adgrowth.internal.integrations.adserver.views
 
-import android.app.Activity
-import android.webkit.JavascriptInterface
-import com.adgrowth.internal.integrations.adserver.helpers.HTMLBuilder
+import android.content.Context
+import android.graphics.drawable.AnimatedImageDrawable
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
+
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.gif.GifDrawable
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class AdImage(
-    context: Activity,
-    private val url: String,
-    private val listener: Listener
-) : WebViewMedia(context) {
+class AdImage(private val context: Context, imageUrl: String, private val listener: Listener) :
+    RequestListener<Drawable> {
     private val mainScope = CoroutineScope(Dispatchers.Main)
+    private val imageView = ImageView(context).apply {
+        layoutParams = FrameLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        translationZ = 2f
+    }
 
     init {
-        setupWebView(object {
-            @JavascriptInterface
-            fun onImageReady() {
-                mainScope.launch { listener.onImageReady() }
-            }
+        Glide.with(context)
+            .load(imageUrl)
+            .listener(this)
+            .preload()
 
-            @JavascriptInterface
-            fun onClick() {
-                mainScope.launch { listener.onClick() }
-            }
+        imageView.setOnClickListener {
+            listener.onClick()
+        }
 
-            @JavascriptInterface
-            fun onImageError() {
-                mainScope.launch { listener.onImageError() }
+    }
+
+    fun addInto(parent: ViewGroup) {
+        mainScope.launch {
+            if (imageView.parent != null) (imageView.parent as ViewGroup).removeView(imageView)
+            parent.addView(imageView)
+
+            imageView.drawable.let {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P && it is AnimatedImageDrawable) {
+                    it.repeatCount = AnimatedImageDrawable.REPEAT_INFINITE
+                    it.start()
+                }
+
+                if (it is GifDrawable) {
+                    it.setLoopCount(GifDrawable.LOOP_FOREVER)
+                    it.startFromFirstFrame()
+                }
             }
-        })
+        }
+    }
+
+    fun release() {
+        mainScope.launch {
+            Glide.with(context).clear(imageView)
+            imageView.setImageDrawable(null)
+            if (imageView.parent != null) (imageView.parent as ViewGroup).removeView(imageView)
+        }
     }
 
 
-    override fun preload() {
-        html = HTMLBuilder.getImageHTML().replace("\\{media_url\\}".toRegex(), url)
-        super.preload()
+    override fun onLoadFailed(
+        e: GlideException?,
+        model: Any?,
+        target: Target<Drawable>?,
+        isFirstResource: Boolean
+    ): Boolean {
+        listener.onImageError()
+        return false
+    }
+
+    override fun onResourceReady(
+        resource: Drawable?,
+        model: Any?,
+        target: Target<Drawable>?,
+        dataSource: DataSource?,
+        isFirstResource: Boolean
+    ): Boolean {
+        mainScope.launch {
+            imageView.setImageDrawable(resource)
+            listener.onImageReady()
+        }
+        return false
     }
 
     interface Listener {
@@ -44,4 +99,6 @@ class AdImage(
         fun onImageReady()
         fun onImageError()
     }
+
+
 }
--- ad_library/src/main/kotlin/com/adgrowth/internal/integrations/adserver/views/AdPlayer.kt
+++ ad_library/src/main/kotlin/com/adgrowth/internal/integrations/adserver/views/AdPlayer.kt
@@ -1,83 +1,122 @@
 package com.adgrowth.internal.integrations.adserver.views
 
-import android.annotation.SuppressLint
-import android.app.Activity
-import android.webkit.JavascriptInterface
-import com.adgrowth.internal.integrations.adserver.entities.Ad
-import com.adgrowth.internal.integrations.adserver.helpers.HTMLBuilder
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.ViewGroup
+import androidx.media3.common.MediaItem
+import androidx.media3.common.PlaybackException
+import androidx.media3.common.Player
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.util.*
 
-@SuppressLint("ViewConstructor")
-class AdPlayer(
-    context: Activity,
-    private val url: String,
-    private val listener: Listener
-) : WebViewMedia(context) {
+class AdPlayer(private val context: Context, url: String, private val listener: Listener) :
+    Player.Listener {
+    private var playerView: PlayerView? = null
     private val mainScope = CoroutineScope(Dispatchers.Main)
-    var adDuration = Ad.DEFAULT_AD_DURATION
-        private set
+    private val player: ExoPlayer = ExoPlayer.Builder(context).build()
+    private val mediaItem: MediaItem = MediaItem.fromUri(url)
+    private val playerHandler = Handler(Looper.getMainLooper())
+
+    val adDuration: Double get() = player.duration.toDouble()
+
+    private val progressUpdater = object : Runnable {
+        override fun run() {
+            if (player.isPlaying) {
+                listener.onVideoProgressChanged(
+                    player.currentPosition.toDouble(),
+                    player.duration.toDouble()
+                )
+                playerHandler.postDelayed(this, 500)
+            }
+        }
+    }
 
     init {
-        setupWebView(object {
-            @JavascriptInterface
-            fun onVideoReady(duration: Double) {
-                mainScope.launch {
-                    adDuration = duration
-                    listener.onVideoReady(duration)
-                }
-            }
-
-            @JavascriptInterface
-            fun onClick() {
-                mainScope.launch { listener.onClick() }
-            }
-
-            @JavascriptInterface
-            fun onVideoError() {
-                mainScope.launch { listener.onVideoError() }
-            }
-
-            @JavascriptInterface
-            fun onPlay() {
-                mainScope.launch { listener.onPlay() }
-            }
-
-            @JavascriptInterface
-            fun onPause() {
-                mainScope.launch { listener.onPause() }
-            }
-
-            @JavascriptInterface
-            fun onVideoFinished() {
-                mainScope.launch { listener.onVideoFinished() }
-            }
-
-            @JavascriptInterface
-            fun onVideoProgressChanged(position: Double, total: Double) {
-                mainScope.launch { listener.onVideoProgressChanged(position, total) }
-            }
-        })
-    }
-
-
-    override fun preload() {
-        html = HTMLBuilder.getVideoHTML().replace("\\{media_url\\}".toRegex(), url)
-        super.preload()
-    }
-
-    fun setMuted(muted: Boolean) {
-        mWebView?.loadUrl("javascript:setMuted(${if (muted) "true" else "false"})")
+        mainScope.launch {
+            player.addListener(this@AdPlayer)
+            player.setMediaItem(mediaItem)
+            player.prepare()
+        }
     }
 
     fun play() {
-        mWebView?.loadUrl("javascript:play()")
+        mainScope.launch {
+            player.playWhenReady = true
+            playerHandler.post(progressUpdater)
+            listener.onPlay()
+        }
     }
 
     fun pause() {
-        mWebView?.loadUrl("javascript:pause()")
+        mainScope.launch {
+            player.playWhenReady = false
+            playerHandler.removeCallbacks(progressUpdater)
+            listener.onPause()
+        }
+    }
+
+    fun setMuted(mute: Boolean) {
+        mainScope.launch {
+            player.volume = if (mute) 0f else 1f
+        }
+    }
+
+    fun release() {
+        mainScope.launch {
+            playerHandler.removeCallbacks(progressUpdater)
+            if (playerView?.parent != null) (playerView?.parent as ViewGroup).removeView(playerView)
+            player.removeListener(this@AdPlayer)
+            player.release()
+        }
+    }
+
+    fun addInto(parent: ViewGroup) {
+        mainScope.launch {
+            playerView = PlayerView(context).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                useController = false
+                player = this@AdPlayer.player
+                setOnClickListener { listener.onClick() }
+            }
+
+            if (playerView?.parent != null) (playerView?.parent as ViewGroup).removeView(playerView)
+
+            parent.addView(playerView)
+        }
+    }
+
+    override fun onPlaybackStateChanged(playbackState: Int) {
+
+        when (playbackState) {
+            Player.STATE_READY -> listener.onVideoReady(player.duration.toDouble())
+            Player.STATE_ENDED -> listener.onVideoFinished()
+        }
+
+    }
+
+    override fun onIsPlayingChanged(isPlaying: Boolean) {
+
+        if (!isPlaying) {
+            listener.onPause()
+        } else {
+            listener.onPlay()
+        }
+
+    }
+
+
+    override fun onPlayerError(error: PlaybackException) {
+        mainScope.launch {
+            listener.onVideoError()
+        }
     }
 
     interface Listener {
@@ -89,5 +128,4 @@ class AdPlayer(
         fun onVideoError()
         fun onClick()
     }
-
 }
